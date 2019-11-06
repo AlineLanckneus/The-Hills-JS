656cc8e69c5aad21b191c4f7321ea844ba8ebe9d
Aline L <aline.lanckneus@live.be>
2019-11-06 11:19:30 +0100

    challenge square success

--- 2.The-Hills/1.JavaScript-Basics/2.Challenge-Series/02-maths/03-even-square/script.js
+++ 2.The-Hills/1.JavaScript-Basics/2.Challenge-Series/02-maths/03-even-square/script.js
@@ -9,12 +9,20 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+let res = [];
+
 (function() {
 
     document.getElementById("run").addEventListener("click", function() {
 
         // your code here
-
+        arr.forEach(function getSquareNum (e) {
+            res.push(Math.pow(e, 2));
+            
+        })
+        alert(`[${res}]`);
     });
-
+    
 })();
+

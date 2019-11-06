9204c34bcfbef7d21274abe02b136266d10475f6
Aline L <aline.lanckneus@live.be>
2019-11-06 18:37:23 +0100

    can't figure this one out

--- 2.The-Hills/1.JavaScript-Basics/2.Challenge-Series/03-colors/01-change-bcg-one/script.js
+++ 2.The-Hills/1.JavaScript-Basics/2.Challenge-Series/03-colors/01-change-bcg-one/script.js
@@ -9,30 +9,12 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
         
-let red = document.getElementById("red");
-let green = document.getElementById("green");
-let yellow = document.getElementById("yellow");
-let blue = document.getElementById("blue");
 
 
-let button = document.querySelector("button");
+(function () {
 
-(function changeColor() {
 
-    button.addEventListener("click", function(){
 
-        if(red && ()){
-            red.style.backgroundColor = "red";
-        } else if (green){
-            green.style.backgroundColor = "green";
-        } else if (yellow){
-            yellow.style.backgroundColor = "yellow";
-        } else if (blue){
-            blue.style.backgroundColor = "blue";
-        }
-        
-        
-    });
     // your code here
 })();
 
